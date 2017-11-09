742df88e95f0f867f2617ce77d3d75b6af702eb2
Adrian Smuzniak <adriansmuzniak@gmail.com>
2017-11-09 18:08:24 +0100

    layout.html

---
+++ js/spa.js
@@ -0,0 +1,21 @@
+/*
+*spa.js
+*Moduł głównej przestrzeni nazw.
+*/
+
+/*jslint browser : true, continue : true,
+        devel : true, indent : 2, maxerr : 50,
+        newcap : true, nomen : true, plusplus : true,
+        regexp : true, sloppy : true, varse : false,
+        white : true,
+*/
+/*global $. spa*/ 
+
+var spa = (function() {
+	var initModule = function($container) {
+		$container.html(
+			'<h1 style="display:inline-block; margin:25px;">' + 'witaj świecie!' + '</h1>'
+		);
+	};
+	return {initModule:initModule};
+}())
