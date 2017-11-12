bfa1fd6153caf7f8edf8e22480abea062a48c74b
Adrian Smuzniak <adriansmuzniak@gmail.com>
2017-11-12 18:35:24 +0100

    spa.shell.js update

---
+++ js/spa.shell.js
@@ -0,0 +1,48 @@
+/*
+*spa.shell.js
+*Moduł powłoki dla aplikacji SPA.
+*/
+
+/*jslint browser : true, continue : true,
+        devel : true, indent : 2, maxerr : 50,
+        newcap : true, nomen : true, plusplus : true,
+        regexp : true, sloppy : true, varse : false,
+        white : true
+*/
+/*global $. spa*/
+
+spa.shell = (function() {
+//---------ROZPOCZĘCIE SEKCJI ZMIENNYCH ZAKRESU MODUŁU-----------------
+var configMap = {
+    main_html: String()
+    + '<div class="spa-shell-head">'
+     + '<div class="spa-shell-head-logo"></div>'
+     + '<div class="spa-shell-head-acct"></div>'
+     + '<div class="spa-shell-head-search"></div>'
+    + '</div>'
+    + '<div class="spa-shell-main">'
+     + '<div class="spa-shell-main-nav"></div>'
+     + '<div class="spa-shell-main-content"></div>'
+    + '</div>'
+    + '<div class="spa-shell-foot"></div>'
+    + '<div class="spa-shell-chat"></div>'
+    + '<div class="spa-shell-modal"></div>'
+},
+stateMap = { $container: null},
+jqueryMap = {};
+
+setJqueryMap, initModule;
+//--------ZAKOŃCZENIE SEKCJI ZMIENNYCH ZAKRESU MODUŁU----------------
+
+//--------ROZPOCZĘCIE SEKCJI METOD NARZĘDZIOWYCH-----------------
+//--------ZAKOŃCZENIE SEKCJI METOD NARZĘDZIOWYCH-----------------
+
+//--------ROZPOCZĘCIE SEKCJI METOD DOM-------------------------------
+//Rozpoczęcie metody DOM /setJqueryMap/.
+setJqueryMap = function() {
+    var container = stateMap.$container;
+    jqueryMap = {$container:$container};
+};
+//Zakończenie metody DOM /setJqueryMap/
+//--------ZAKOŃCZENIE SEKCJI METOD DOM--------------------------------
+})
